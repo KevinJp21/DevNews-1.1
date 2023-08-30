0e3597e2383096b7c0e4903f77c2d58f61f81cf9
KevinJp21 <kevinjp821@gmail.com>
2023-08-30 15:58:08 -0500

    Updates

--- assets/index-54cc5bc6.js
+++ assets/index-54cc5bc6.js
@@ -146,8 +146,33 @@ const t = [
     },
   ],
 
+  /*---------------------Content------------------------*/
+  c = [{ h2: "Contenido", h1: "Publicaciones recientes" }],
 
+  /*---------------------publicaciones recientes-----------------------*/
+  u = [
+    {
+      id: "1",
+      img_1:
+        "https://appmarketingnews.io/wp-content/uploads/2023/03/Captura-de-Pantalla-2023-03-21-a-las-14.25.02.jpg",
+      link_post_1: "index.html",
+      tittle_post_1: "Los lenguajes de programación más demandadosen 2023",
+      author_name: "Kevin Julio Pineda",
+      date: "03 de agosto de 2023",
+    },
+    {
+      id: "2",
+      img_1:
+        "https://static.wixstatic.com/media/96edd2_cb4e4247d69b497bab9ddf024913b501~mv2.jpg/v1/fit/w_900%2Ch_505%2Cal_c%2Cq_80/file.jpg",
+      link_post_1: "/post_2.html",
+      tittle_post_1:
+        "Los mejores frameworks frontend de 2023 para desarrollo web",
+      author_name: "Kevin Julio Pineda",
+      date: "03 de agosto de 2023",
+    },
+  ],
 
+  p = { post: t, post2: d, content_title: c, content: u },
   /*---------------------POST 1------------------------*/
   m = p.post.map(
     (e) => `
